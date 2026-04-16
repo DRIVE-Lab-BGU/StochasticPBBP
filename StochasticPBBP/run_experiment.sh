5e3343c47196f8b6f2a19c260356c73561df790c
ataitler <ataitler@gmail.com>
2026-04-16 18:28:20 +0300

    run experiment

--- StochasticPBBP/run_experiment.sh
+++ StochasticPBBP/run_experiment.sh
@@ -13,7 +13,7 @@ noisetype=$6
 noisestd=$7
 output=$8
 
-runai submit -i registry.bgu.ac.il/hpc/jupyter-notebook:latest -e HOME=/gpfs0/bgu-ataitler/users/ataitler --name "$test" -g 0.5 --cpu-limit 4 -- "cd ~/code/StochasticBPPB && source activate ~/env/StochasticBPPB && python run.py --domain ${domain} --instance ${i} --seeds ${seeds} --eval ${evals} --iterations ${iters} --noisetype ${noisetype} --noisestd ${noisestd} --output ${output} -e"
+runai submit -i registry.bgu.ac.il/hpc/jupyter-notebook:latest -e HOME=/gpfs0/bgu-ataitler/users/ataitler --name "$test" -g 0.5 --cpu-limit 4 -- "cd ~/code/StochasticBPPB && source activate ~/env/StochasticBPPB && python -m StochasticBPPB.run --domain ${domain} --instance ${i} --seeds ${seeds} --eval ${evals} --iterations ${iters} --noisetype ${noisetype} --noisestd ${noisestd} -e"
 
 
 # Run the Python script $count times
